aeceeeae529f0e8b28954e4dd15bcc554dddb19f
Jose <jfontenla.glez@gmail.com>
2024-09-05 00:49:54 +0200

    Add transactions, and comunicate with CoinCap

---
+++ Controllers/MarketDataController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoboTrader.Domain.Repositories;
+using RobotTrader.Application;
+using RobotTrader.Application.DTOs;
+using System.Text.Json;
+
+namespace RobotTrader.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarketDataController : ControllerBase
+    {
+        private readonly ICryptoMarketDataProvider _cryptoMarketDataProvider;
+
+        public MarketDataController(ICryptoMarketDataProvider cryptoMarketDataProvider)
+        {
+            _cryptoMarketDataProvider = cryptoMarketDataProvider;
+        }
+
+        [HttpGet("assets")]
+        public async Task<IActionResult> GetAssets()
+        {
+
+            var assetsStrings = await _cryptoMarketDataProvider.GetAllAssetsAsync();
+
+            var assetsDto = JsonSerializer.Deserialize<AssetsRootDto> (assetsStrings);
+
+            return Ok(assetsDto);
+        }
+
+        [HttpGet("assets/{name}")]
+        public async Task<IActionResult> GetAssets(string name)
+        {
+            var assetString = await _cryptoMarketDataProvider.GetMarketDataByNameAsync(name);
+
+            var assetDto = JsonSerializer.Deserialize<AssetRootDto>(assetString);
+
+            return Ok(assetDto);
+        }
+
+        /// <summary>
+        /// Para saber el valor de conversion de una moneda fiat o cripto
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("rates/{name}")]
+        public async Task<IActionResult> GetRate(string name)
+        {
+            var assetString = await _cryptoMarketDataProvider.GetMarketDataByNameAsync(name);
+
+            var assetDto = JsonSerializer.Deserialize<AssetRootDto>(assetString);
+
+            return Ok(assetDto);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using RoboTrader.Domain.Repositories;
+using RobotTrader.Infrastructure;
+using RobotTrader.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    }
+);
+var externalUrl = builder.Configuration.GetSection("ExternalApiSettings:CoinCapApiUrl");
+builder.Services.Configure<ExternalApiSettings>(builder.Configuration.GetSection("ExternalApiSettings:CoinCapApiUrl"));
+builder.Services.AddHttpClient<ICryptoMarketDataProvider,CoinCapMarketDataProvider>();
+
+
+builder.Services.AddScoped<ITradeRepository,TradeRepository>();
+
 
 // Add services to the container.
 
